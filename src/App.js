a1f1d5968d1aa3df78d8f8837a400c688da786c3
alienfoxx <alienfoxx001@gmail.com>
2024-10-18 20:19:30 +0100

    add the hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Header,Container, Footer } from "./components/index"
+
+import { Header,Container, Footer, Hero } from "./components/index"
 
 const App = () => {
   return (
     <>
        <Header/>
        <Container/>
-       <Footer/>
+       <Hero/>
+       {/* <Footer/> */}
     </>
   )
 }
